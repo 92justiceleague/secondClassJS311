d3930cf3ad3987458f6d464c2f97ff0441ee1ea5
Alaysha <alaysha_justice@yahoo.com>
2022-11-09 20:53:59 -0500

    second push

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let express = require('express')
 
 let app = express ()
 
-let PORT = 8000;
+let PORT = 8001;
 
 app.get("/hello" , function (req, res){
     res.send("hello") //send back hello string
@@ -13,7 +13,11 @@ app.post("/bye", function(){
     res.send ("later gator");
 })
 
-app.listen(PORT, function(){}{
+app.get("/bye", function(){
+    res.send("afterwhile crocodile!");
+})
+
+app.listen(PORT, function(){
     console.log("application start on port", PORT);
 })
 
