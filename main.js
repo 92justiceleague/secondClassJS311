c1a2849e8857faf34b647609cfabeef2bb2cddc9
Alaysha <alaysha_justice@yahoo.com>
2022-11-09 21:46:05 -0500

    third push

--- main.js
+++ main.js
@@ -2,14 +2,25 @@ let express = require('express')
 
 let app = express ()
 
-let PORT = 8001;
+app.use(express.json())
+
+let PORT = 7000;
 
 app.get("/hello" , function (req, res){
-    res.send("hello") //send back hello string
+
+    let query = req.query;
+    let greeting = "hello";
+    if(query.name){
+        greeting = "hello there " +query.name;
+    }
+    console.log("Query: ", query);
+        res.send(greeting) //sends back string
 })
 
 
 app.post("/bye", function(){
+    let payload = req.body;
+    console.log("Request body =", payload);
     res.send ("later gator");
 })
 
